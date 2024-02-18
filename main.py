a2ba7903649d5d2c9c350382f44fd42bdc97c30c
hendrixperalta <hendrixperalta@gmail.com>
2024-02-18 18:36:25 +0900

    Changed functionality
    The stopwatch functionality is embedded into the button class

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
 import time
+import button
+import menu
 
 value = ""
 topic = ""
-print("Press enter when you start studying")
-
-# while value.lower() != "q":
-#     value = input()
-#
-#     total_time = round((time.time() - start_time), 2)
-#
-#     print("You Finished !")
-#     print("total time " + str(total_time))
-#
-#     print("*"*20)
-#     #finish_time = time.time()
-#
-#
-# print("exercise complete")
+print("Press y to count")
 
 value = input()
-start_time = time.time()
-print("Time running //")
 
+if value !="":
+    button.stopwatch()
 
-value = input()
-total_time = round((time.time() - start_time), 2)
-
-print("This is the total time for:" + str(topic))
-print("total time " + str(total_time) + "s")
-
-print("*" * 20)
-# finish_time = time.time()
-
-print("exercise complete")
