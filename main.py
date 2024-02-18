49b08a0f9f6c6df23a3c1684f27e226f4f114e8a
hendrixperalta <hendrixperalta@gmail.com>
2024-02-18 18:52:16 +0900

    Record Stopwatch time
    The program is capable to iterate on the stopwatch and record the times

--- main.py
+++ main.py
@@ -4,10 +4,24 @@ import menu
 
 value = ""
 topic = ""
-print("Press y to count")
+time_list = []
+time_spent = 0
 
-value = input()
+while value != "q":
+    print("Press any key to count / press q to exit")
+    value = input()
 
-if value !="":
-    button.stopwatch()
+    if value != "q":
+        time_spent = button.stopwatch()
 
+        print("Do you want to save this time   y/n")
+        save_time = input().lower()
+
+        if save_time == "n":
+            print("Time not saved")
+            print(time_list)
+
+        else:
+            time_list.append(time_spent)
+            print("Time Saved !!!")
+            print(time_list)
